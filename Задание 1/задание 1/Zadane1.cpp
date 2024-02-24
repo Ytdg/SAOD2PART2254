2ddf938279f8683e2dbac0b1107cf23cbdd31f49
Ytdg <76431656+Ytdg@users.noreply.github.com>
2024-02-24 23:01:50 +0300

    Add files via upload

---
+++ Задание 1/задание 1/Zadane1.cpp
@@ -0,0 +1,10 @@
+﻿
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hellow World!" << endl;
+}
+
---
+++ Задание 1/задание 1/module1.cpp
@@ -0,0 +1,13 @@
+﻿
+
+#include <iostream>;
+#include "utils.h"
+using namespace std;
+int main()
+{
+   
+    int x = 3, y = 5;
+    cout << Plus(x, y)<< endl;
+    
+}
+
