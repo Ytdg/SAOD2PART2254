61380c7be0d9c9eee0b34de32395373f1a2adcb3
Ytdg <76431656+Ytdg@users.noreply.github.com>
2024-02-27 19:54:06 +0300

    Add files via upload

---
+++ Задание 2/задание 2/Task2.cpp
@@ -0,0 +1,26 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+void Swap(int &a, int &b) {
+    int c = a;
+    a = b;
+    b = c;
+    
+}
+int main()
+{   /*
+    typedef unsigned char byte;
+    int x = -1;
+    int *p = &x;
+    byte* pb = (byte*)--p;		
+    for (byte* pt = pb; pt - pb < sizeof(int); pt++)
+        cout << (int)*pt << ' ';
+    cout << endl;
+    */
+    int a = 3, b = 5;
+    Swap(a, b);
+    cout << a << b<<' ';
+   
+}
